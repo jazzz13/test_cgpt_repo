2aaad173fec9e0be629f1330b60fef296247a317
jazzz13 <murtazin13@gmail.com>
2024-06-10 14:45:18 +0500

    Update index.js to setup TypeORM and REST methods

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const http = require('http');
+const express = require('express');
+const { createConnection } = require('typeorm');
+const bodyParser = require('body-parser');
+const Post = require('./entity/Post');
 
-const app = http.createServer((req, res) => {
-  res.end('Hello, World!');
-});
-
+const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Test_cgpt_repo app listening on port ${PORT}`);
-});
+app.use(bodyParser.json());
+
+createConnection({
+  type: 'sqlite',
+  database: 'database.sqlite',
+  synchronize: true,
+  logging: true,
+  entities: [Post],
+})
+  .then((connection) => {
+    const postRepository = connection.getRepository('Post');
+
+    app.post('/posts', async (req, res) => {
+      const { title, content } = req.body;
+      const post = postRepository.create({ title, content });
+      await postRepository.save(post);
+      res.status(201).json(post);
+    });
+
+    app.get('/posts', async (req, res) => {
+      const posts = await postRepository.find();
+      res.json(posts);
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Test_cgpt_repo app listening on port ${PORT}`);
+    });
+})
+  .catch((error) => console.log(error));
+
